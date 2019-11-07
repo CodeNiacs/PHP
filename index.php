1704cd7468c82862406d6f0ac13b75e986a908d0
Codeniacs <info@codeniacs.nl>
2019-11-07 04:10:38 +0100

    Markup improved

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
+<?php
+    function info()
+    {
+        ob_start();
+        phpinfo( INFO_GENERAL );
+        $info = ob_get_clean();
+
+        $info = preg_replace("/^.*?\<body\>/is", "", $info);
+        $info = preg_replace("/<\/body\>.*?$/is", "", $info);
+
+        echo $info;
+    }
+?>
 <!DOCTYPE html>
 <html>
+<head>
+    <style>
+    body
+    {
+        background-color: orange;
+    }
+
+    h1
+    {
+        color: white;
+        text-align: center;
+    }
+
+    p
+    {
+        font-family: verdana;
+        font-size: 20px;
+    }
+</style>
+</head>
 <body>
-    <h1>Hack PHP informatie over de editor die je gebruikt</h1>
-  <pre><p><?php phpinfo(); ?></p></pre>
+    <h1>Hack informatie over deze editor</h1>
+    <pre><p><?php info(); ?></p></pre>
 </body>
 </html>
